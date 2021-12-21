4bf4b0fa950be871c453c70207d7131f8bf803a0
dhse22 <stardhse@gmail.com>
2021-12-21 10:49:11 -0500

    front

---
+++ src/pages/Index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Index = () => (
+  <div>
+    <div className='bg-green-300 h-96'>
+      Esta es una pagina que esta en estado de desarrollo, test 2
+    </div>
+    <div className='bg-green-300 h-96'>Index page</div>
+    <div className='bg-green-300 h-96'>Index page</div>
+    <div className='bg-green-300 h-96'>Index page</div>
+  </div>
+);
+
+export default Index;
